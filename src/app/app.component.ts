13abb8b4da7d68efddf6b9f86ca4c515ae89930c
Pouria Rezaei <pouria.rezaei1992@gmail.com>
2024-01-21 21:36:23 +0330

    add vercel analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import {Component} from "@angular/core";
 import {CacheService} from "rxjs-cache-service";
 import {outerLinks} from "./core/constants/outer-links";
-
+import {inject as injectVercelAnalyticsScript} from "@vercel/analytics";
 @Component({
    selector: "app-root",
    templateUrl: "./app.component.html",
 })
 export class AppComponent {
+   protected readonly outerLinks = outerLinks;
+
    constructor(private _cacheService: CacheService) {
+      injectVercelAnalyticsScript();
+      // accessing the cache to help early initialization
       const cachedData = this._cacheService.cachedData;
    }
-
-   protected readonly outerLinks = outerLinks;
 }
