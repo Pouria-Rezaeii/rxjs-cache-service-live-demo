706914ce07464923e33dfb6de786827e8e1d888e
Pouria Rezaei <pouria.rezaei1992@gmail.com>
2024-01-30 13:57:18 +0330

    update skeleton styles

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {CacheService} from "rxjs-cache-service";
 import {HttpClient} from "@angular/common/http";
 import {Post} from "../core/types/post.type";
-import {delay, map} from "rxjs";
+import {delay} from "rxjs";
 import {ActivatedRoute} from "@angular/router";
 import {DEFAULT_PAGE_SIZE} from "../core/constants/default-page-size";
 
@@ -17,20 +17,24 @@ export class PostsService {
    public getPosts(params: {start?: number; limit?: number; userId?: number}) {
       const {start, limit, userId} = params;
 
-      return this._cache.get<Post[]>({
-         url: "posts",
-         defaultParams: {_start: 0, _limit: DEFAULT_PAGE_SIZE, _sort: "title"},
-         params: {_start: start, _limit: limit, userId: userId},
-         observable: ({arrangedUrl}) => this._httpClient.get<Post[]>(arrangedUrl).pipe(delay(300)),
-         refresh: true,
-      });
+      return this._cache
+         .get<Post[]>({
+            url: "posts",
+            defaultParams: {_start: 0, _limit: DEFAULT_PAGE_SIZE, _sort: "title"},
+            params: {_start: start, _limit: limit, userId: userId},
+            observable: ({arrangedUrl}) => this._httpClient.get<Post[]>(arrangedUrl),
+            refresh: true,
+         })
+         .pipe(delay(300));
    }
 
    public getSinglePost(id: number) {
-      return this._cache.get<Post>({
-         url: `posts/${id}`,
-         observable: ({arrangedUrl}) => this._httpClient.get<Post>(arrangedUrl).pipe(delay(300)),
-         clearTimeout: 30000,
-      });
+      return this._cache
+         .get<Post>({
+            url: `posts/${id}`,
+            observable: ({arrangedUrl}) => this._httpClient.get<Post>(arrangedUrl),
+            clearTimeout: 30000,
+         })
+         .pipe(delay(300));
    }
 }
