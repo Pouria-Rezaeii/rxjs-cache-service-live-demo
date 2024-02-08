22a249644230cb2f343ddfd1689ecf8f1c1de6fe
Pouria Rezaei <pouria.rezaei1992@gmail.com>
2024-02-08 23:30:22 +0330

    fix: chain delay pipe to observable to prevent further delays

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -17,24 +17,20 @@ export class PostsService {
    public getPosts(params: {start?: number; limit?: number; userId?: number}) {
       const {start, limit, userId} = params;
 
-      return this._cache
-         .get<Post[]>({
-            url: "posts",
-            defaultParams: {_start: 0, _limit: DEFAULT_PAGE_SIZE, _sort: "title"},
-            params: {_start: start, _limit: limit, userId: userId},
-            observable: ({arrangedUrl}) => this._httpClient.get<Post[]>(arrangedUrl),
-            refresh: true,
-         })
-         .pipe(delay(300));
+      return this._cache.get<Post[]>({
+         url: "posts",
+         defaultParams: {_start: 0, _limit: DEFAULT_PAGE_SIZE, _sort: "title"},
+         params: {_start: start, _limit: limit, userId: userId},
+         observable: ({arrangedUrl}) => this._httpClient.get<Post[]>(arrangedUrl).pipe(delay(300)),
+         refresh: true,
+      });
    }
 
    public getSinglePost(id: number) {
-      return this._cache
-         .get<Post>({
-            url: `posts/${id}`,
-            observable: ({arrangedUrl}) => this._httpClient.get<Post>(arrangedUrl),
-            clearTimeout: 30000,
-         })
-         .pipe(delay(300));
+      return this._cache.get<Post>({
+         url: `posts/${id}`,
+         observable: ({arrangedUrl}) => this._httpClient.get<Post>(arrangedUrl).pipe(delay(300)),
+         clearTimeout: 30000,
+      });
    }
 }
